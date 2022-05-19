d921fc9b04315314a29bda9fec5f65941913a84e
Dipanjan Kailthya <dipanjan.kailthya@TMNL.nl>
2022-05-19 12:59:25 +0200

    Initial Commit

---
+++ src/python_codeartifact_demo/app.py
@@ -0,0 +1,14 @@
+
+import logging
+logger = logging.getLogger("python_codeartifact_demo")
+
+class App:
+    
+    def __init__(self, settings):
+        self.settings = settings    
+
+    def start(self):
+        logger.info("python_codeartifact_demo Starting")
+
+    def stop(self):
+        logger.info("python_codeartifact_demo Stopping")
---
+++ tests/python_codeartifact_demo/test_sample.py
@@ -0,0 +1,8 @@
+# vim:fenc=utf-8
+from click.testing import CliRunner
+import python_codeartifact_demo
+from python_codeartifact_demo import cli
+
+
+def test_true():
+    assert 1 == 1
